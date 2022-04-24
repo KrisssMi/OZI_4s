85d9fa33d2e03196170f86f61dc3f8ce8072fbfc
Kristina <87947917+KrisssMi@users.noreply.github.com>
2022-04-24 17:40:09 +0300

    ADD

---
+++ Отчёт_6/Lab_6/Lab_6/Diffi.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Lab_6
+{
+    public static class Power
+    {
+        public static uint power(uint a, uint b, uint n)
+        {// a^b mod n
+            uint tmp = a;
+            uint sum = tmp;
+            for (int i = 1; i < b; i++)
+            {
+                for (int j = 1; j < a; j++)
+                {
+                    sum += tmp;
+                    if (sum >= n)
+                    {
+                        sum -= n;
+                    }
+                }
+                tmp = sum;
+            }
+            return tmp;
+        }
+
+    }
+    public class PrimeNumberGenerator
+    {
+        private readonly Random _random = new Random();
+        public uint Generate()
+        {
+
+            uint number = (uint)_random.Next(300);
+
+            while (!IsPrime(number))
+            {
+                unchecked
+                {
+                    number++;
+                }
+            }
+            return number;
+        }
+
+        private static bool IsPrime(uint number)
+        {
+            if ((number & 1) == 0) return (number == 2);
+
+            var limit = (int)Math.Sqrt(number);
+            for (int i = 3; i <= limit; i += 2)
+            {
+                if ((number % i) == 0) return false;
+            }
+            return true;
+        }
+    }
+    class Alice
+    {
+        public static uint primeNumber;
+        public static uint aliceNumber;
+        private static uint secretKey;
+        public static uint openKey;
+        private static uint generalSecretKey;
+
+        public static void GenerateNumber()
+        {
+            Random ranGenerator;
+            ranGenerator = new Random();
+            aliceNumber = (uint)ranGenerator.Next((int)primeNumber);
+            Bob.aliceNumber = aliceNumber;
+        }
+        public static void GenerateSecretKey()
+        {
+            Random ranGenerator;
+            ranGenerator = new Random();
+            secretKey = (uint)ranGenerator.Next((int)primeNumber);
+        }
+        public static void GenerateOpenKey()
+        {
+            openKey = Power.power(aliceNumber, secretKey, primeNumber);
+        }
+        public static void GenerateGeneralSecretKey()
+        {
+            generalSecretKey = Power.power(Bob.openKey, secretKey, primeNumber);
+        }
+        public static void Output()
+        {
+            Console.WriteLine($"Случайное число меньшее {primeNumber}: {aliceNumber}");
+            Console.WriteLine($"Секретный ключ Алисы {secretKey}");
+            Console.WriteLine($"Открытый ключ Алисы {openKey}");
+            Console.WriteLine($"Общий секретный ключ, который получила Алиса {generalSecretKey}");
+        }
+    }
+    public static class Bob
+    {
+        public static uint primeNumber;
+        public static uint aliceNumber;
+        private static uint secretKey;
+        public static uint openKey;
+        private static uint generalSecretKey;
+        public static void GeneratePrimeNumber()
+        {
+            PrimeNumberGenerator generator;
+            generator = new PrimeNumberGenerator();
+            primeNumber = generator.Generate();
+            Alice.primeNumber = primeNumber;
+        }
+        public static void GenerateSecretKey()
+        {
+            Random ranGenerator;
+            ranGenerator = new Random();
+            secretKey = (uint)ranGenerator.Next((int)primeNumber);
+        }
+        public static void GenerateOpenKey()
+        {
+            openKey = Power.power(aliceNumber, secretKey, primeNumber);
+        }
+        public static void GenerateGeneralSecretKey()
+        {
+            generalSecretKey = Power.power(Alice.openKey, secretKey, primeNumber);
+        }
+        public static void Output()
+        {
+            Console.WriteLine($"Простое число: {primeNumber}");
+            Console.WriteLine($"Секретный ключ Боба: {secretKey}");
+            Console.WriteLine($"Открытый ключ Боба: {openKey}");
+            Console.WriteLine($"Общий секретный ключ, который получил Боб: {generalSecretKey}");
+        }
+    }
+    public class DiffieHellman
+    {
+        public DiffieHellman()
+        {
+            Bob.GeneratePrimeNumber();
+            Alice.GenerateNumber();
+            Bob.GenerateSecretKey();
+            Thread.Sleep(1000);
+            Alice.GenerateSecretKey();
+            Bob.GenerateOpenKey();
+            Alice.GenerateOpenKey();
+            Bob.GenerateGeneralSecretKey();
+            Alice.GenerateGeneralSecretKey();
+            Bob.Output();
+            Alice.Output();
+        }
+    }
+}
---
+++ Отчёт_6/Lab_6/Lab_6/ElGamal.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Lab_6
+{
+    public class ElGamal
+    {
+        public int p;
+        public int x;
+        public int g;
+        public int y;
+
+        public string input;
+        public List<string> encoded;
+        public string decoded;
+        private readonly Random _random = new Random();
+
+        public int GetPRoot(int p)
+        {
+            for (int i = 0; i < p; i++)
+                if (IsPRoot(p, i))
+                    return i;
+            return 0;
+        }
+
+        public bool IsPRoot(int p, int a)
+        {
+            if (a == 0 || a == 1)
+                return false;
+            int last = 1;
+            HashSet<int> set = new HashSet<int>();
+            for (int i = 0; i < p - 1; i++)
+            {
+                last = (last * a) % p;
+                if (set.Contains(last)) // Если повтор
+                    return false;
+                set.Add(last);
+            }
+            return true;
+        }
+        public int Power(int a, int b, int n)
+        {// a^b mod n 
+            int tmp=a; 
+            int sum=tmp; 
+            for(int i=1;i<b;i++)
+            { 
+                for(int j=1;j<a;j++)
+                { sum+=tmp;
+                    if(sum>=n)
+                    { 
+                        sum-=n;
+                    } 
+                } tmp=sum;
+            } 
+            return tmp;
+        }
+           
+        public int Mul(int a, int b, int n)
+        {// a*b mod n 
+            int sum=0; 
+            for(int i=0;i<b;i++)
+            { 
+                sum+=a; 
+                if(sum>=n)
+                {
+                    sum-=n;
+                } 
+            } 
+            return sum; 
+        }
+        public ElGamal()
+        {
+            bool ok = false;
+            while(ok == false){
+                p = _random.Next(1000);
+                int l = GetPRoot(p);
+                g = l;
+                if (g > 0)
+                    ok = true;
+            }
+            x = _random.Next(1, p - 1);
+
+            y = Power(g, x, p);
+            Console.WriteLine($"Открытые ключи: g = {g}, p = {p}, y = {y}        {x} "); //
+            Console.WriteLine("Введите шифруемый текст");
+            input = Console.ReadLine();
+
+            encoded = Encode(p, g, x, input);
+
+            foreach (string i in encoded)
+                Console.WriteLine(i);
+
+            decoded = Decode(p, x, encoded);
+
+            Console.WriteLine(decoded);
+        }
+
+        public List<string> Encode(int p, int g, int x, string input)
+        {
+            List<string> result = new List<string>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                int k = _random.Next(1, p - 1);
+                Console.WriteLine($"Сессионный ключ: k = {k}");
+                int m = Convert.ToInt32(input[i]);
+                Console.WriteLine(m);
+                int a = Power(g, k, p);
+                int b = Mul(Power(y, k, p), m, p);
+                result.Add($"{a},{b}");
+            }
+            return result;
+        }
+        public string Decode(int p, int x, List<string> input)
+        {
+            string result = "";
+            foreach(string item in input)
+            {
+                string[] nums = item.Split(',');
+                int a = int.Parse(nums[0]);
+                int b = int.Parse(nums[1]);
+
+                int dM = Mul(b, Power(a, p - 1 - x, p), p);
+
+                result += (char)dM;
+            }
+            return result;
+        }
+    }
+}
